e7016f313006da01332fe35eed01801a5ade6f9a
Paul Tyng <paul@paultyng.net>
2020-08-31 13:59:50 -0400

    Add well known error for required_version issues

--- tfexec/cmd.go
+++ tfexec/cmd.go
@@ -132,7 +132,7 @@ func (tf *Terraform) runTerraformCmd(cmd *exec.Cmd) error {
 
 	err := cmd.Run()
 	if err != nil {
-		return parseError(err, errBuf.String())
+		return tf.parseError(err, errBuf.String())
 	}
 	return nil
 }
